7b19117ef696f74826975d2b34e811bf858600fc
Kartik Thakur <kartikthakur029@gmail.com>
2023-11-07 15:43:25 +0530

    update

--- utils/endpoint.js
+++ utils/endpoint.js
@@ -1,2 +1,2 @@
 // export const endpoint = 'http://localhost:3000/api/'
-export const endpoint = 'https://familyguy-quiz.vercel.app/api/'
+export const endpoint = 'https://familyguy-quiz-app.vercel.app/api/'
